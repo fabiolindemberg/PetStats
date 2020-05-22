56c8cc8c7fd012e083496f06bd40034ff6a7c557
Fábio Silva <fabio.silva@madeinweb.app>
2020-05-22 20:28:53 -0300

    MainScreen - Adding alpha view to highlight labels

--- PetStats/ViewController.swift
+++ PetStats/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var vwGradientAlpha: UIView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        config()
         // Do any additional setup after loading the view.
     }
 
-
+    func config() {
+        
+//        let gradient = CAGradientLayer(start: .topLeft, end: .bottomRight, colors: [UIColor.black.withAlphaComponent(0).cgColor, UIColor.black.withAlphaComponent(1).cgColor], type: .axial)
+        let gradient = CAGradientLayer()
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.colors = [UIColor.black.withAlphaComponent(0.0).cgColor, UIColor.black.withAlphaComponent(0.9).cgColor]
+        gradient.type = .axial
+        gradient.frame = vwGradientAlpha.bounds
+        vwGradientAlpha.layer.addSublayer(gradient)
+    }
 }
 
